55c07dbce3ee40425e8df80227d79ca192438c50
Dominik <zawlockidominik@gmail.com>
2025-12-07 18:01:33 +0100

    GUI: print file path where fine tune data has been saved

--- godot_project/src/fine_tune_data_saver.gd
+++ godot_project/src/fine_tune_data_saver.gd
@@ -3,10 +3,12 @@ extends Node
 const FINE_TUNE_SHEET_FILENAME = "fine_tune_data.csv"
 var _file: FileAccess
 
+var _fine_tune_sheet_path: String
+
 
 func _ready() -> void:
-	var fine_tune_sheet_path = Settings.model_folder_path.path_join(FINE_TUNE_SHEET_FILENAME)
-	_file = FileAccess.open(fine_tune_sheet_path, FileAccess.WRITE)
+	_fine_tune_sheet_path = Settings.model_folder_path.path_join(FINE_TUNE_SHEET_FILENAME)
+	_file = FileAccess.open(_fine_tune_sheet_path, FileAccess.WRITE)
 	if _file == null:
 		print("Error, cant open fine tune sheet: ", FileAccess.get_open_error())
 		return
@@ -16,9 +18,9 @@ func save_row(data: PackedStringArray, ground_truth: int) -> void:
 	print("Abut to save")
 	if _file == null:
 		return
-		
-	print("almost saving...")
 	
 	data.insert(0, str(ground_truth))
 	_file.store_csv_line(data)
 	_file.flush()
+	
+	print("saved to ", _fine_tune_sheet_path)
