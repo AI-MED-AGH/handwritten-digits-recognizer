e8ef9a7609e3ad7640273de145089e057682143e
Dominik <zawlockidominik@gmail.com>
2025-12-09 17:19:57 +0100

    Add static typing in method headers of ClassifierServer

--- predict.py
+++ predict.py
@@ -12,14 +12,14 @@ class ClassifierServer(MLController):
     model_name = "handwritten-digits-recognizer"
     model_version = "1.0.0"
 
-    def load_model(self):
+    def load_model(self) -> MyModel:
         PATH = "trained_models/model.pth"
         model = MyModel()
         model.load_state_dict(torch.load(PATH, weights_only=True))
         return model
 
     @preprocessing
-    def preprocess(self, data) -> torch.Tensor:
+    def preprocess(self, data: list[list[int]]) -> torch.Tensor:
         X = np.array(data, dtype=np.float32) / 255.0
         X = X.reshape((-1, 1, 28, 28))
         X_tensor = torch.from_numpy(X)
@@ -33,14 +33,14 @@ class ClassifierServer(MLController):
             return output
 
     @postprocessing
-    def postprocess(self, probabilities) -> list:
+    def postprocess(self, probabilities: torch.Tensor) -> list:
         probabilities: np.ndarray = np.exp(probabilities.numpy())
         predicted_labels = np.argmax(probabilities, axis=1)
 
         response = [
             {
                 "label": label.tolist(),
-                "proba": proba.tolist()
+                "proba": proba.tolist(),
             }
             for label, proba in zip(predicted_labels, probabilities)
         ]
