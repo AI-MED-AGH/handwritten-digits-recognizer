b8c92e44923aea166851d32d1e091a96be6648fd
Rafał Mazur <raf.mazur.000@gmail.com>
2026-04-05 20:52:11 +0200

    New model architecture

---
+++ Recognizer.py
@@ -0,0 +1,39 @@
+from torch import nn, Tensor
+import torch.nn.functional as func
+
+class Recognizer(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # 1st convolution
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, padding=2),
+            nn.ReLU(),
+            
+            # 2nd convolution
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, padding=2),
+            nn.ReLU(),
+            
+            # 3rd convolution withc maxPooling
+            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            
+            # Flatten data in order to feed it to linear layers
+            nn.Flatten(),
+
+            # 1st linear layer
+            nn.Linear(in_features=14*14, out_features=128), 
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+
+            # 2nd linera layer
+            nn.Linear(in_features=128, out_features=32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+
+            # output layer with one shadow class
+            nn.Linear(in_features=32, out_features=11),
+
+            nn.LogSoftmax()
+        )
