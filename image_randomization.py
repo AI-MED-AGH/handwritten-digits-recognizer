be252d48d69f738af6c5fcdd5660c0e6c5026cc4
Rafał Mazur <raf.mazur.000@gmail.com>
2026-04-06 01:29:39 +0200

    Created image randomization, begun to insert blanks into datasets and created directory to hold blank images

---
+++ image_randomization.py
@@ -0,0 +1,13 @@
+import torch
+
+# TODO:
+class RandomizeImage:
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwds):
+        pass
+    
+
+class AddArtifacts(RandomizeImage):
+    pass
