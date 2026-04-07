0814b6384ccbcfca3339d3765de2de0ced730f09
Rafek731 <raf.mazur.000@gmail.com>
2026-04-07 16:35:49 +0200

    New model architecture (RecognizerV2)

--- models/Recognizer.py
+++ models/Recognizer.py
@@ -37,3 +37,42 @@ class RecognizerV1(nn.Sequential):
 
             nn.LogSoftmax()
         )
+
+class RecognizerV2(nn.Sequential):
+    def __init__(self, *args):
+        super().__init__(
+            # 1st convolution
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, padding=2),
+            nn.BatchNorm2d(10),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2), # shape : 10 x 14 x 14 
+            
+            # 2nd convolution
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, padding=2),
+            nn.BatchNorm2d(20),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2), # shape : 20 x 7 x 7 
+
+            # 3rd convolution
+            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=5, padding=2),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            
+            # Flatten data in order to feed it to linear layers
+            nn.Flatten(), # shape : 20 x 7 x 7
+
+            # 1st linear layer
+            nn.Linear(in_features=280, out_features=128), 
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+
+            # 2nd linear layer
+            nn.Linear(in_features=128, out_features=32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+
+            # output layer with one shadow class
+            nn.Linear(in_features=32, out_features=11),
+        )
